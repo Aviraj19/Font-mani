0e5b7ff4b8fef50557c6212021851d2093172bb4
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-04-19 17:00:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex=""
+nosey=""
+leftwristx=""
+rightwristx=""
+difference=""
 function setup() {
     canvas = createCanvas(680, 500)
     canvas.position(1100, 200)
@@ -10,13 +15,20 @@ function setup() {
 function modelloaded() {
     console.log("poseNet is loaded")
 }
-
-function draw() {
-    background("gray")
-}
-
 function gotposes(results) {
     if (results.length > 0) {
         console.log(results)
+        nosex=results[0].pose.nose.x
+        nosey=results[0].pose.nose.y
+        leftwristx=results[0].pose.leftWrist.x
+        rightwristx=results[0].pose.rightWrist.x
+        difference=floor(leftwristx-rightwristx)
     }
+}
+function draw() {
+    background("gray")
+    fill("red")
+    textSize(difference)
+    text("Aviraj",nosex,nosey)
+    document.getElementById("differ").innerHTML=difference
 }
